4b7d8e9934f886415fb51cd1cbc493a9ca1e8a61
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-30 18:27:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,11 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-phrase = "It's-a me, Mario!"
-
 def mario
+  phrase = "It's-a me, Mario!"
   puts phrase
   phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
   status
 end
